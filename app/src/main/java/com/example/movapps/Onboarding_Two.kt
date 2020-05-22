c6a0b8fedee999370a8a26109c998c704dfea85b
Hummam Aly Baziyad <huhummamalybaziyad@gmail.com>
2020-05-22 21:07:10 +0700

    finish onboarding screen.

---
+++ app/src/main/java/com/example/movapps/Onboarding_Two.kt
@@ -0,0 +1,12 @@
+package com.example.movapps
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Onboarding_Two : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.onboarding__two)
+    }
+}
