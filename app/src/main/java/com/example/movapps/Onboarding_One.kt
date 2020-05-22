c70d5a9b57ddf390b5a96e644dee8d45688247e1
Hummam Aly Baziyad <huhummamalybaziyad@gmail.com>
2020-05-22 20:39:01 +0700

    Initial commit

---
+++ app/src/main/java/com/example/movapps/Onboarding_One.kt
@@ -0,0 +1,12 @@
+package com.example.movapps
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Onboarding_One : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.onboarding__one)
+    }
+}
---
+++ app/src/main/java/com/example/movapps/Splash_Screen.kt
@@ -0,0 +1,12 @@
+package com.example.movapps
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Splash_Screen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splash_screen)
+    }
+}
