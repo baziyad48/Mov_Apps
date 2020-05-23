9a0a5a1ac3764a5a59f45f03061d2e002c1b484e
Hummam Aly Baziyad <huhummamalybaziyad@gmail.com>
2020-05-23 19:21:44 +0700

    finish sign up avatar screen.

---
+++ app/src/main/java/com/example/movapps/Sign_Up_Photo.kt
@@ -0,0 +1,12 @@
+package com.example.movapps
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Sign_Up_Photo : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.sign__up__photo)
+    }
+}
