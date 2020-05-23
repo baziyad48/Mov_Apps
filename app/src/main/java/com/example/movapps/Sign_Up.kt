7e3e805ae41a1d95829059a310af4a6f6015a8a2
Hummam Aly Baziyad <huhummamalybaziyad@gmail.com>
2020-05-23 14:03:44 +0700

    finish sign up screen.

---
+++ app/src/main/java/com/example/movapps/Sign_Up.kt
@@ -0,0 +1,12 @@
+package com.example.movapps
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Sign_Up : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.sign__up)
+    }
+}
