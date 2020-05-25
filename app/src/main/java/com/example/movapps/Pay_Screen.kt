b1e155b696a8bae48e9d348110be657136b3d8a7
Hummam Aly Baziyad <huhummamalybaziyad@gmail.com>
2020-05-25 14:55:29 +0700

    finish pay screen.

---
+++ app/src/main/java/com/example/movapps/Pay_Screen.kt
@@ -0,0 +1,12 @@
+package com.example.movapps
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Pay_Screen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.pay__screen)
+    }
+}
