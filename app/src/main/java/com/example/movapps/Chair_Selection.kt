0357f90f095541074b0e5d2ec2f29d213bc94b5f
Hummam Aly Baziyad <huhummamalybaziyad@gmail.com>
2020-05-25 11:28:18 +0700

    finish movie details (minus recyclerview).

---
+++ app/src/main/java/com/example/movapps/Chair_Selection.kt
@@ -0,0 +1,12 @@
+package com.example.movapps
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Chair_Selection : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.chair__selection)
+    }
+}
---
+++ app/src/main/java/com/example/movapps/Movie_Details.kt
@@ -0,0 +1,12 @@
+package com.example.movapps
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Movie_Details : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.movie__details)
+    }
+}
